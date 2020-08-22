6ed266536a6064ae09c9c3af53a69a2d9b63e8ad
chez-shanpu <cheztomo513@gmail.com>
2020-08-23 00:38:04 +0900

    Add mail classifier using GCP

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module github.com/StudioAquatan/hacku2020
 go 1.15
 
 require (
+	cloud.google.com/go v0.46.3
 	github.com/emersion/go-imap v1.0.5
 	github.com/emersion/go-imap-idle v0.0.0-20200601154248-f05f54664cc4
 	github.com/emersion/go-message v0.12.0
-	github.com/mitchellh/go-homedir v1.1.0
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/slack-go/slack v0.6.6
+	github.com/golang/protobuf v1.4.2
 	github.com/onsi/ginkgo v1.14.0
 	github.com/onsi/gomega v1.10.1
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.6.6
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
 	golang.org/x/sys v0.0.0-20200819091447-39769834ee22 // indirect
+	google.golang.org/genproto v0.0.0-20191108220845-16a3f7862a1a
 )
--- pkg/email/classifier.go
+++ pkg/email/classifier.go
@@ -1,6 +1,15 @@
 package email
 
-import "strings"
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	language "cloud.google.com/go/language/apiv1"
+	"github.com/golang/protobuf/proto"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+)
 
 var oinoriWordsSubj = []string{
 	"選考結果",
@@ -30,4 +39,36 @@ func ClassifyMailByBody(body string) bool {
 		}
 	}
 	return false
+}
+
+func ClassifyMailBySentiment(s string) bool {
+	ctx := context.Background()
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := analyzeSentiment(ctx, client, s)
+	if resp != nil && resp.DocumentSentiment.Score < 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func analyzeSentiment(ctx context.Context, client *language.Client, text string) (*languagepb.AnalyzeSentimentResponse, error) {
+	return client.AnalyzeSentiment(ctx, &languagepb.AnalyzeSentimentRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_Content{
+				Content: text,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+	})
+}
+
+func printResp(v proto.Message, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	proto.MarshalText(os.Stdout, v)
 }
