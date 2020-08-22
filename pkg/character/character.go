1c1fc06d82284f945984af829c08f82b43417a2e
chez-shanpu <cheztomo513@gmail.com>
2020-08-22 18:03:41 +0900

    Update run command

---
+++ pkg/character/character.go
@@ -0,0 +1,62 @@
+package character
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Info struct {
+	Name    string
+	Icon    string
+	Message []string
+}
+
+type MessageInfo struct {
+	Name    string
+	Icon    string
+	Message string
+}
+
+func (ci *Info) removeMessage(index int) {
+	var ms []string
+	for i, v := range ci.Message {
+		if i == index {
+			continue
+		}
+		ms = append(ms, v)
+	}
+	ci.Message = ms
+}
+
+func removeCharacter(cis *[]Info, index int) []Info {
+	var newCis []Info
+	for i, v := range *cis {
+		if i == index {
+			continue
+		}
+		newCis = append(newCis, v)
+	}
+	return newCis
+}
+
+func CreateMessageInfoByRandom(cis []Info, messageNum int) *[]MessageInfo {
+	var mis []MessageInfo
+
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < messageNum; i++ {
+		characterIndex := rand.Intn(len(cis))
+		messageIndex := rand.Intn(len(cis[characterIndex].Message))
+		mi := MessageInfo{
+			Name:    cis[characterIndex].Name,
+			Icon:    cis[characterIndex].Icon,
+			Message: cis[characterIndex].Message[messageIndex],
+		}
+		mis = append(mis, mi)
+		cis[characterIndex].removeMessage(messageIndex)
+		if len(cis[characterIndex].Message) == 0 {
+			cis = removeCharacter(&cis, characterIndex)
+		}
+	}
+
+	return &mis
+}
--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -6,26 +6,28 @@ import (
 	"github.com/slack-go/slack"
 )
 
-type MessageInfo struct {
+type SlackMessageInfo struct {
 	Api       slack.Client
 	ChannelID string
 	UserName  string
 	IconEmoji string
+	Message   string
 }
 
-func NewMessageInfo(token, channelId, userName, iconEmoji string) *MessageInfo {
-	return &MessageInfo{
+func NewSlackMessageInfo(token, channelId, userName, iconEmoji, message string) *SlackMessageInfo {
+	return &SlackMessageInfo{
 		Api:       *slack.New(token),
 		ChannelID: channelId,
 		UserName:  userName,
 		IconEmoji: iconEmoji,
+		Message:   message,
 	}
 }
 
-func (i *MessageInfo) PostMessage(t string) error {
+func (i *SlackMessageInfo) PostMessage() error {
 	if _, _, err := i.Api.PostMessage(
 		i.ChannelID,
-		slack.MsgOptionText(t, false),
+		slack.MsgOptionText(i.Message, false),
 		slack.MsgOptionIconEmoji(i.IconEmoji),
 		slack.MsgOptionUsername(i.UserName),
 		slack.MsgOptionAsUser(false),
